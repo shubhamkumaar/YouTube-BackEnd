26f685cc2d5e588ece35f8ecaef36d53e752c01c
shubhamkumaar <shubhamkumarjhadss8@gmail.com>
2024-03-13 01:10:02 +0530

    Add get user profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -334,6 +334,74 @@ const updateUserCoverImage = asyncHandler(async(req,res) =>{
     )
 
 })
+
+const getUserChannelProfie = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(400,"Username is missing")
+    }
+    const channel =await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{
+                    $size:"$subscribers"
+                },
+                channelSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $con:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscriberCount:1,
+                channelSubscribedToCount:1,
+                isSubscribed:1,
+                email:1,
+                avatar:1,
+                coverImage:1
+            }
+        }
+    ])
+    if(!channel.length){
+        throw new ApiError(404,"Channel does not Exist")
+    }
+    console.log(channel);
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User profile fetched successfully")
+    )
+})
 export {
     registerUser,
     loginUser,
@@ -343,5 +411,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfie // pending to check
 }
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -11,4 +11,4 @@ const subscriptionSchema = new Schema({
     }
 },{timestamps:true})
 
-export const subscription = mongoose.model("Subcription",subscriptionSchema)
+export const subscription = mongoose.model("Subscription",subscriptionSchema)
