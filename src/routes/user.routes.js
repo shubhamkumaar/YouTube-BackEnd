eeda591f976014aa53556a4a07d5fc283ee5466c
Shubham Kumar <115333492+shubhamkumaar@users.noreply.github.com>
2024-03-19 22:23:43 +0530

    add watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfie, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -24,9 +24,11 @@ router.route("/login").post(loginUser)
 // Secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/update-avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/update-password").post(verifyJWT,changeCurrentPassword)
-router.route("/update-details").post(verifyJWT,updateAccountDetails)
+router.route("/update-details").patch(verifyJWT,updateAccountDetails)
 router.route("/get-user").get(verifyJWT,getCurrentUser) 
-router.route("/update-cover-image").post(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfie)
+router.route("/watch-history").get(verifyJWT,getWatchHistory)
 export default router
